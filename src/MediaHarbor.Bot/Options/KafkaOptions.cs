76c928a3a66d023758c3595cf76a7afbc9b79eae
Maksym Fedorenko <fedorenko.mx@gmail.com>
2024-01-03 21:26:53 +0200

    Fix docker-compose.yaml

---
+++ src/MediaHarbor.Bot/Options/KafkaOptions.cs
@@ -0,0 +1,6 @@
+namespace MediaHarbor.Bot.Options;
+
+public class KafkaOptions
+{
+    public string BootstrapServers { get; set; } = null!;
+}
--- src/MediaHarbor.Bot/Program.cs
+++ src/MediaHarbor.Bot/Program.cs
@@ -1,6 +1,7 @@
 using Confluent.Kafka;
 using MediaHarbor.Bot;
 using MediaHarbor.Bot.Domain.DataLake;
+using MediaHarbor.Bot.Options;
 using MediaHarbor.Bot.UpdatePipelines;
 using MediaHarbor.Logger;
 using SimpleKafka;
@@ -9,10 +10,13 @@ using TBot.Core.CallLimiter;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddKafkaProducer<string>(new ProducerConfig { BootstrapServers = "localhost" });
+var kafkaOptions = new KafkaOptions();
+builder.Configuration.Bind(nameof(KafkaOptions), kafkaOptions);
+
+builder.Services.AddKafkaProducer<string>(new ProducerConfig { BootstrapServers = kafkaOptions.BootstrapServers });
 builder.Services.AddKafkaConsumersFactory();
 builder.Services.AddDependencies();
-builder.Logging.AddLogger(builder.Configuration, new KafkaSettings { BootstrapServers = "localhost" });
+builder.Logging.AddLogger(builder.Configuration, new KafkaSettings { BootstrapServers = kafkaOptions.BootstrapServers });
 
 builder.AddTBot(botBuilder =>
 {
